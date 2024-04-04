0a3172bf6114d5e7619a18b1706816c3720d8a97
! Art <124545712+artexss@users.noreply.github.com>
2024-04-04 02:59:20 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# GoodBot-source-code
+https://discord.gg/2KehbsZevH OfFIciAL SOurCe CoDes
---
+++ index.ts
@@ -0,0 +1,1 @@
+console.log('Detailed setup instructions and code in description: https://www.youtube.com/watch?v=dQw4w9WgXcQ')
